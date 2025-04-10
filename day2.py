324bd98e40d5606abd1b3f98023268756fd07135
YUSSIF ABDUL-RAHAMAN <abdulrahamanyussif62@gmail.com>
2025-04-10 15:01:14 +0000

    Create day2.py

---
+++ day2.py
@@ -0,0 +1,9 @@
+names = ['Yussif', 'Babamu', 'Abdul', 'Abdulazeez', 'Abdulwahab', 'Abdulmalik', 'Abdulrahman', 'Abdulkareem', 'Abdullahi', 'Abdullahi']
+print('names:', names)
+
+vegetables = ['carrot', 'potato', 'onion', 'cabbage', 'spinach']
+print('vegetables:', vegetables)
+animals = ['cat', 'dog', 'elephant', 'lion', 'tiger']
+print('animals:', animals) 
+mixedtypes = ['apple', 1, 2.5, True, None]
+print('mixedtypes:', mixedtypes)
